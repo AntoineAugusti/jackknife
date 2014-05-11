fe290fe455e17f8317a9cd74ce9d1fa40510c338
Antoine Augusti <antoine@augusti.fr>
2014-05-11 16:07:14 +0200

    Code : affichage du tableau + partie décision à propos stabilité

--- code/jackknifeMoyenne.m
+++ code/jackknifeMoyenne.m
@@ -4,10 +4,8 @@ mu = 2;
 sigma = 1;
 echantillons = [62 78 79 80 82 82 83 85 87 91 96 97 97 97 101 120 135 180 270 400];
 nbEchantillons = length(echantillons);
-echantillons
-disp(['* La moyenne des échantillons : ', num2str(mean(echantillons))]);
 
-% Calcul des moyennes partielles
+% Calcul des moyennes réduites
 moyennesReduites = [];
 for i=1:nbEchantillons
 	echantillonReduit = echantillons;
@@ -16,7 +14,10 @@ for i=1:nbEchantillons
 	moyennesReduites = [moyennesReduites mean(echantillonReduit)];
 	disp(['Moyenne partielle sans le ', int2str(i), 'ème élément : ', num2str(mean(echantillonReduit))]);
 end
+tableau = [echantillons' moyennesReduites'];
+tableau
 
+disp(['* La moyenne des échantillons : ', num2str(mean(echantillons))]);
 disp(['* La moyenne des moyennes réduites est de : ', num2str(mean(moyennesReduites))]);
 disp(['* Intervalle de confiance de 100 % sur les moyennes réduites : [', num2str(moyennesReduites(end)), '; ', num2str(moyennesReduites(1)), ']'])
 disp(['* Intervalle de confiance de 90 % sur les moyennes réduites : [', num2str(moyennesReduites(end-1)), '; ', num2str(moyennesReduites(2)), ']'])
