6962e6911d7ff2c9dc5654fea6d074a8c45c6972
Ansart Manon <mansart@asi-testing>
2014-05-14 13:12:18 +0200

    Ajout d'une boucle pour le calcul de la variance

--- code/variance.m
+++ code/variance.m
@@ -3,18 +3,24 @@ close all;
 
 moyenne = 10;
 variance = 6;
-n = 20;
+n = 500;
 
-data= randn(n,1).*sqrt(variance) .+ moyenne;
+data= randn(n,1).*sqrt(variance)+moyenne;
 
-varianceTh = var(data)
-varianceEst = sum((data .- moyenne).^2)/n
+varianceTh=var(data)
+varianceEst = sum((data - moyenne).^2)/n
 
 %varianceJackknife
 dataech = zeros(n-1);
 for i=1:n
 	dataech = [data(1:i-1); data(i+1:end)];
-	pseudoval(i) = n*varianceEst - (n-1) * sum((dataech .- mean(dataech)).^2)/(n-1);
+	moyenneEch = mean(dataech);
+	varianceEch = 0;
+	for j=1:n-1
+		varianceEch = varianceEch+(dataech(j) - moyenneEch).^2;
+	end
+	varianceEch =  varianceEch/(n-1);
+	pseudoval(i) = n*varianceEst - (n-1) *varianceEch;
 end
 
 varianceJackknife = mean(pseudoval)
